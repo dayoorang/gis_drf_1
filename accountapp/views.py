ed54222550e76729d50ee2312ff5b1f1b1699281
dayoorang <dayoorang@gmail.com>
2021-12-09 14:35:57 +0900

    hello_world input data return

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
+
+    if request.method == "POST":
+        input_data = request.POST.get('input_data')
+        return render(request,'accountapp/hello_world.html', context={'message':input_data})
+
     return render(request,'accountapp/hello_world.html')
