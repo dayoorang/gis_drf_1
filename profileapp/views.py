249fe2599404072441c2784ba295088cc41c1391
dayoorang <dayoorang@gmail.com>
2021-12-15 11:40:24 +0900

    Profileapp create view form_valid update

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+
+from profileapp.form import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.owner.pk})
